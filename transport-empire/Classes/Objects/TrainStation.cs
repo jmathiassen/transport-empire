e5e1578b779f0d1682c851e1b9c27a6cb285c213
Jan Martin Mathiassen <85899578+jmathiassen@users.noreply.github.com>
2022-07-21 08:45:33 +0200

    f

---
+++ transport-empire/Classes/Objects/TrainStation.cs
@@ -0,0 +1,17 @@
+﻿using TransportEmpire.Classes.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportEmpire.Classes.Objects
+{
+	internal class PopulationCenter
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Population { get; set; }
+		public List<Industry> Industries { get; set; }
+	}
+}
